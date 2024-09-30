7a15207ebf4872919d5a19504444fa9d7ab6ef8b
Daniel Clavijo Coca <dclavijo@opennebula.io>
2024-09-29 21:13:55 -0600

    Add doc redirect

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from fastapi.responses import RedirectResponse
 from fastapi import FastAPI, status, HTTPException, Header, Path, Depends
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from typing import Annotated, Any, List
@@ -19,6 +20,12 @@ one.ONE_XMLRPC = conf.ONE_XMLRPC
 
 app = FastAPI(title='Cognit Frontend', version='0.1.0')
 
+
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/docs")
+
+
 @app.post("/v1/authenticate", status_code=status.HTTP_201_CREATED)
 async def authenticate(credentials: Annotated[HTTPBasicCredentials, Depends(HTTPBasic())]) -> str:
     one.authenticate(credentials.username,
@@ -26,6 +33,7 @@ async def authenticate(credentials: Annotated[HTTPBasicCredentials, Depends(HTTP
 
     token = auth.generate_token(credentials.username, credentials.password)
     return token
+
 
 @app.get("/v1/public_key", status_code=status.HTTP_200_OK)
 async def get_public_key() -> str:
@@ -94,7 +102,7 @@ async def get_edge_cluster_frontends(
                             'Content-Type': 'application/json'})
     body = response.json()
 
-    cluster_ids = body['ID'] # AI orchestrator returns an array of IDs
+    cluster_ids = body['ID']  # AI orchestrator returns an array of IDs
     clusters = []
 
     for cluster_id in cluster_ids:
